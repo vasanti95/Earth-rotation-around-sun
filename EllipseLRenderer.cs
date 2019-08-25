1b09a857f5dbc560ff286a766b656911f7762935
vasanti <vasanti95chavan@gmail.com>
2019-08-25 11:47:08 +0530

    First Commit

---
+++ EllipseLRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(LineAlignment))]
+public class EllipseLRenderer : MonoBehaviour {
+
+    public LineRenderer lr;
+
+    public Ellipse elipse;
+
+    [Range(0, 100)]
+    public int segments;
+
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        CalculateElipse();
+    }
+
+    //Generate eliptical shape
+    void CalculateElipse()
+    {
+        Vector3[] points = new Vector3[segments + 1];
+        for (int i = 0; i < segments; i++)
+        {
+
+            Vector2 Pos2d = elipse.Evaluate((float)i / (float)segments);
+            points[i] = new Vector3(Pos2d.x,0f, Pos2d.y);
+        }
+        points[segments] = points[0];
+
+        lr.positionCount = segments + 1;
+        lr.SetPositions(points);
+    }
+
+    private void OnValidate()
+    {
+        if(Application.isPlaying && lr!=null)
+        {
+            CalculateElipse();
+        }
+       
+    }
+
+
+}
---
+++ PlanetRotation.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour {
+
+    public static PlanetRotation instance;
+
+    public Transform orbitingObject;
+    public Ellipse orbitPath;
+
+    public float orbitSpeed;
+
+    [Range(0,1)]
+    public float orbitProgress=0;
+
+    public float orbitPeriod;
+    public static bool orbitActive;
+    //public float planetDistance = 19.83f;
+
+
+	//for Earth speed required to rotate around sun=(distance travelled/secound)/Time
+	// Here we need timeperiod means orbit period
+	// so Time=ditance/speed
+	// time(orbitPeriod)=149.6*10^6(distance traveled by earth)/29.8(earth speed)
+	// orbitSpeed=1f/orbitPeriod
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        if (orbitingObject == null)
+        {
+            orbitActive = false;
+        }
+        else
+        {
+            orbitActive = true;
+        }
+        //set orbitingObject position
+        SetOrbitingObjectPosition();
+        //if(earthobject) start animation orbitingObject
+        StartCoroutine(AnimateOrbit());
+    }
+
+    void SetOrbitingObjectPosition()
+    {
+        Vector2 orbitPos = orbitPath.Evaluate(-orbitProgress);
+        orbitingObject.localPosition = new Vector3(orbitPos.x, 0, orbitPos.y);
+    }
+
+    public IEnumerator AnimateOrbit()
+    {
+        if (orbitPeriod <= 1f)
+        {
+            orbitPeriod = 0.1f;
+        }
+        orbitSpeed = (1f / orbitPeriod);
+
+        Debug.Log("orbit active : "+orbitActive);
+
+        while (orbitActive)
+        {
+            orbitProgress += Time.deltaTime * orbitSpeed;
+            orbitProgress %= 1f;
+            SetOrbitingObjectPosition();
+            yield return null;
+        }
+
+    }
+
+}
